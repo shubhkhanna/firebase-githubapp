dfe85ea15da568f70dfd5b0240a1f4e6c94554ab
shubhkhanna <khannashubh04@gmail.com>
2020-07-29 17:49:26 +0530

    Updates

---
+++ precache-manifest.fc7472b00afb1b04eba50ea4e8624db2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25c0a5af9a198ed944b5f57f8263d57",
+    "url": "/firebase-githubapp/index.html"
+  },
+  {
+    "revision": "21c4109504c8440c699e",
+    "url": "/firebase-githubapp/static/css/2.79dd3d24.chunk.css"
+  },
+  {
+    "revision": "6caebbd39bf87a937bad",
+    "url": "/firebase-githubapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "21c4109504c8440c699e",
+    "url": "/firebase-githubapp/static/js/2.ec2cf490.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/firebase-githubapp/static/js/2.ec2cf490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6caebbd39bf87a937bad",
+    "url": "/firebase-githubapp/static/js/main.3c678e01.chunk.js"
+  },
+  {
+    "revision": "c7dfc6d3c499170563d9",
+    "url": "/firebase-githubapp/static/js/runtime-main.90f30351.js"
+  }
+]);
